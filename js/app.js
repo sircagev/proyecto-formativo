a2bd9df3ffc939171656f728f6c444cb3c9b71d0
sircagev <cf.giron04@gmail.com>
2023-04-17 22:10:49 -0500

    ficha tecnica

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-function añadirCaracteristica(){
-    
-}
+const caracteristicas = document.getElementById('caracteristicas')
+const agregar = document.getElementById('agregar')
+
+let total = 2
+
+agregar.addEventListener('click', e => {
+    let div = document.createElement('div')
+    div.innerHTML = `<span>Característica ${total++}</span> <input type='text' placeholder="Característica">`
+    caracteristicas.appendChild(div)
+})
